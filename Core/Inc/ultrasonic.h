653a9997eed7ff67a5969b60b77a63bcc2421f58
subject-a-lacan <193558087@qq.com>
2026-03-09 20:25:15 +0800

    sr04_version1_done

--- Core/Inc/ultrasonic.h
+++ Core/Inc/ultrasonic.h
@@ -2,7 +2,16 @@
 #define __ULTRASONIC_H
 
 #include "main.h"
+// 引脚宏定义，方便修改
+#define SR04_TRIG_PORT   GPIOA
+#define SR04_TRIG_PIN    GPIO_PIN_0
 
+#define SR04_ECHO_PORT   GPIOA
+#define SR04_ECHO_PIN    GPIO_PIN_1
+
+void SR04_Init(void);
+uint16_t SR04_GetDistance(void);
+void SR04_Test(void);
 
 
 #endif
