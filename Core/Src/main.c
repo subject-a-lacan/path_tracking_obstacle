1cbf72fea2899c7f4da3635c163cc06ed4f36792
subject-a-lacan <193558087@qq.com>
2026-03-30 21:49:05 +0800

    可以开始调试循迹了

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -122,7 +122,7 @@ PID_t error={
     .Target = 0,
     .Actual = 0,
     .Out = 0,
-    .Kp = 0.0f,
+    .Kp = 5.0f,
     .Ki = 0.0f,
     .Kd = 0.0f,
     .Error_now = 0,
@@ -140,8 +140,8 @@ typedef enum {
     CAR_STATE_LOST_LINE_GO = 1,// 丢线直行状态
     CAR_STATE_OBSTACLE_AVOID = 2// 避障状态
 } CarState;
-// volatile CarState car_state = CAR_STATE_TRACKING; // 初始化为循迹状态
-volatile CarState car_state=CAR_STATE_LOST_LINE_GO;//PID调参测试专用
+volatile CarState car_state = CAR_STATE_TRACKING; // 初始化为循迹状态
+// volatile CarState car_state=CAR_STATE_LOST_LINE_GO;//PID调参测试专用
 
 //超声波+速度传感
 volatile int16_t  track_error = 0;    // 灰度传感器计算出的偏航偏差
@@ -312,17 +312,15 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
             {
                 case 0: // CAR_STATE_TRACKING (循迹模式)
                     // 1. 动态降速：误差越大，基础速度越慢 (安全过弯)
-                    // base_speed = 100 - (33 * abs(track_error) / 1024);
-                    // if (base_speed < 100) base_speed = 100; // 兜底最低速度
-                    
+                    base_speed = 100 - (33 * abs(track_error) / 1024);
+                    if (base_speed < 250) base_speed = 250; // 兜底最低速度
                     // // 2. 转向环 PID 计算
-                    // yaw.Target = 0;
-                    // yaw.Actual = track_error;
-                    // PID_Update(&yaw);
-                    
+                    yaw.Target = 0;
+                    yaw.Actual = track_error;
+                    PID_Update(&yaw);
                     // // 3. 差速分配给左右轮目标速度
-                    // target_L = base_speed + (int16_t)yaw.Out;
-                    // target_R = base_speed - (int16_t)yaw.Out;
+                    target_L = base_speed + (int16_t)yaw.Out;
+                    target_R = base_speed - (int16_t)yaw.Out;
                     break;
 
                 case 1: // CAR_STATE_LOST_LINE_GO (丢线直行)
@@ -358,12 +356,9 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
             
             // 1. 计算左轮速度环 PID
             speed_L.Target = target_L;
-           
             PID_Update(&speed_L);
-            
             // 2. 计算右轮速度环 PID
             speed_R.Target = target_R;
-           
             PID_Update(&speed_R);
             if(speed_L.Out>400)speed_L.Out=400;
             if(speed_R.Out>400)speed_R.Out=400; 
@@ -537,8 +532,8 @@ int main(void)
     // Left_Speed_Proc(&left_speed);    这两行代码是大错特错 读取速度必须在中断函数里 否则数据不是实时的！
     // Right_Speed_Proc(&right_speed); 
     // SR04_Proc(&front_distance);
-    // Gray_Proc(&sensor, Normal, &track_error);
-    // Digtal=Get_Digtal_For_User(&sensor); 
+    Gray_Proc(&sensor, Normal, &track_error);
+    Digtal=Get_Digtal_For_User(&sensor); 
     // StateMachine_Update(); // 根据当前传感器数据和状态机逻辑更新小车状态
     /* USER CODE END WHILE */
 
