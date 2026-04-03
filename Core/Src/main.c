bd3d2f650eb604d85434dfc666c9e03dfa6d4e45
subject-a-lacan <193558087@qq.com>
2026-04-03 16:59:47 +0800

    ?

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -144,8 +144,8 @@ typedef enum {
     CAR_STATE_LOST_LINE_GO = 1,// 丢线直行状态
     CAR_STATE_OBSTACLE_AVOID = 2// 避障状态
 } CarState;
-volatile CarState car_state = CAR_STATE_TRACKING; // 初始化为循迹状态
-// volatile CarState car_state=CAR_STATE_LOST_LINE_GO;//PID调参测试专用
+// volatile CarState car_state = CAR_STATE_TRACKING; // 初始化为循迹状态
+volatile CarState car_state=CAR_STATE_LOST_LINE_GO;//PID调参测试专用
 
 //超声波+速度传感
 volatile int16_t  track_error = 0;    // 灰度传感器计算出的偏航偏差
@@ -512,7 +512,7 @@ int main(void)
   Encoder_Init();
   No_MCU_Ganv_Sensor_Init(&sensor,white,black); 
   // ESP8266_Init("F521F520","f521f520","192.168.100.15","8080");   //这是Gong的
-  ESP8266_Init("F521F520","f521f520","192.168.100.43","8080");   //这是Xu的
+  ESP8266_Init("F521F520","f521f520","192.168.100.27","8080");   //这是Xu的
   Steer_SetAngle(90);
   // IMU_init();         
   // HAL_Delay(10);
