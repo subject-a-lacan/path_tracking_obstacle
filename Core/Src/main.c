aa51b995ebe3e684cc946f78e5e17a50c525a7fb
subject-a-lacan <193558087@qq.com>
2026-04-03 16:48:06 +0800

    ?

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -517,7 +517,7 @@ int main(void)
   Encoder_Init();
   No_MCU_Ganv_Sensor_Init(&sensor,white,black); 
   // ESP8266_Init("F521F520","f521f520","192.168.100.15","8080");   //这是Gong的
-  ESP8266_Init("F521F520","f521f520","192.168.100.43","8080");   //这是Xu的
+  ESP8266_Init("F521F520","f521f520","192.168.100.27","8080");   //这是Xu的
   Steer_SetAngle(90);
   // IMU_init();         
   // HAL_Delay(10);
